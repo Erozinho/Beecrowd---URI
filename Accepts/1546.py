694e2b01a456861f4e63aff46910ca0c56891db6
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-13 12:03:54 -0300

    Create 1546.py

---
+++ Accepts/1546.py
@@ -0,0 +1,13 @@
+A = int(input())
+for i in range(A):
+    B = int(input())
+    for i in range(B):
+        C = int(input())
+        if C == 1:
+            print("Rolien")
+        elif C == 2:
+            print("Naej")
+        elif C == 3:
+            print("Elehcim")
+        elif C == 4:
+            print("Odranoel")
