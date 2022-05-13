67687c73894997cee6850c0d117c252ac3532575
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-13 10:15:35 -0300

    Create 1015.py

---
+++ Accepts/1015.py
@@ -0,0 +1,7 @@
+import math
+linha1 = input().split(" ")
+linha2 = input().split(" ")
+x1,y1 = linha1
+x2,y2 = linha2
+raiz = math.sqrt(((float(x2) - float(x1))*(float(x2) - float(x1))) + ((float(y2)-float(y1)) *(float(y2)-float(y1))))
+print("{:.4f}".format(raiz))
