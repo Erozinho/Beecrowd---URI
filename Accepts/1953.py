ce88a23215cd37ca91f3fa789893d04d6c5f298f
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-14 12:32:55 -0300

    Create 1953.py

---
+++ Accepts/1953.py
@@ -0,0 +1,16 @@
+while true:
+    try:
+        N = int(input())
+        EPR, EHD, INTRUSOS = 0, 0, 0
+        for X in range(N):
+            RM,CURSO = input().split()
+            RM = int(RM)
+            if CURSO == "EPR":
+                EPR += 1
+            elif CURSO == "EHD":
+                EHD += 1
+            else:
+                INTRUSOS += 1
+        print("EPR: {}\nEHD: {}\nINTRUSOS: {}".format(EPR,EHD,INTRUSOS))
+    except EOFError:
+        break
