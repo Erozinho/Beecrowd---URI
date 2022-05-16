f08fad4ea913df0b81fa63902c89a2324934a4f5
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-15 23:13:38 -0300

    Delete 1165.py

--- Accepts/1165.py
+++
@@ -1,7 +0,0 @@
-N = int(input())
-for Y in range(N):
-    X = int(input())
-    if (X % X == 0) and (X % 2 != 0) and (X % 3 != 0):
-        print("{} eh primo".format(X))
-    else:
-        print("{} nao eh primo".format(X))
