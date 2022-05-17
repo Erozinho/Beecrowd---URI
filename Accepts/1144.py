be5a3032456687b67e68b5ba704937e6a0df21db
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-17 12:35:01 -0300

    Update 1144.py

--- Accepts/1144.py
+++ Accepts/1144.py
@@ -1,6 +1,5 @@
 N,A,A1 = int(input()),1,1
 for X in range(1, N + 1):
-    print("{} {} {}".format(A,A*A,A*A1))
-    print("{} {} {}".format(A,(A*A)+1,(A*A1)+1))
+    print("{} {} {}\n{} {} {}".format(A,A*A,A*A1,A,(A*A)+1,(A*A1)+1))
     A += 1
     A1 = A * A
