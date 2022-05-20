68891c98977985c87556250e52a6aadc6cb46073
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 14:45:10 -0300

    Update 1043.py

--- Accepts/1043.py
+++ Accepts/1043.py
@@ -1,9 +1,16 @@
-NUM = input().split()
-A, B, C = NUM
-A = float(A)
-B = float(B)
-C = float(C)
+A, B, C = map(float,input().split())
 if abs(B - C) < A < (B + C) and (A - C) < B < (A + C) and (A - B) < C < (A + B):
     print("Perimetro = {:.1f}".format(A + B + C))
 else:
     print("Area = {:.1f}".format(((A + B) / 2) * C))
+# Codigo velho:
+
+#NUM = input().split()
+#A, B, C = NUM
+#A = float(A)
+#B = float(B)
+#C = float(C)
+#if abs(B - C) < A < (B + C) and (A - C) < B < (A + C) and (A - B) < C < (A + B):
+    #print("Perimetro = {:.1f}".format(A + B + C))
+#else:
+    #print("Area = {:.1f}".format(((A + B) / 2) * C))
