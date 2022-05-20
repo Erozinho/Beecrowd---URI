1daf7b3285b5289dca0c679f87e7ad17b295f79c
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 13:50:29 -0300

    Update 1017.py

--- Accepts/1017.py
+++ Accepts/1017.py
@@ -1,3 +1,8 @@
-T = int(input())
-V = int(input())
+T,V = int(input()),int(input())
 print("{:.3f}".format((T * V)/12))
+
+# Codigo velho:
+
+#T = int(input())
+#V = int(input())
+#print("{:.3f}".format((T * V)/12))
