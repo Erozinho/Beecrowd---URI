057ee2feec78d096d21c7de68be1f03cfaeb3d50
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 14:28:09 -0300

    Update 1042.py

--- Accepts/1042.py
+++ Accepts/1042.py
@@ -1,6 +1,12 @@
-NUM = list(map(int, input().strip().split()))
-for V in sorted(NUM):
-    print(V)
-print()
-for V in NUM:
-    print(V)
+NUM = list(map(int,input().split()))
+X = sorted(NUM)
+print("{}\n{}\n{}\n\n{}\n{}\n{}".format(X[0],X[1],X[2],NUM[0],NUM[1],NUM[2]))
+
+# Codigo velho:
+
+#NUM = list(map(int, input().strip().split()))
+#for V in sorted(NUM):
+    #print(V)
+#print()
+#for V in NUM:
+    #print(V)
