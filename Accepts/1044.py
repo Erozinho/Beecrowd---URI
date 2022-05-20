dbebe1a7c9751a4d90f110a925b59335e58db0f9
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 18:01:49 -0300

    Update 1044.py

--- Accepts/1044.py
+++ Accepts/1044.py
@@ -1,13 +1,22 @@
 A,B = map(int,input().split())
-if A > B:
-    if A % B == 0:
-        print("Sao Multiplos")
-    else:
-        print("Nao sao Multiplos")
-if A < B:
-    if B % A == 0:
-        print("Sao Multiplos")
-    else:
-        print("Nao sao Multiplos")
-if A == B:
+if A > B and A % B == 0 or A == B:
     print("Sao Multiplos")
+if A < B and B % A == 0:
+    print("Sao Multiplos")
+elif A < B and B % A != 0 or A > B and A % B != 0:
+    print("Nao sao Multiplos")
+    
+# Codigo velho:
+
+#A,B = map(int,input().split())
+#if A > B and A % B == 0:
+        #print("Sao Multiplos")
+#else:
+      #print("Nao sao Multiplos")
+#if A < B:
+    #if B % A == 0:
+        #print("Sao Multiplos")
+    #else:
+        #print("Nao sao Multiplos")
+#else:
+    #print("Sao Multiplos")
