e41defc67871b229209e85b401fd5c1d595cecfe
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 14:34:08 -0300

    Update 1038.py

--- Accepts/1038.py
+++ Accepts/1038.py
@@ -1,12 +1,11 @@
-NUM = input().split()
-C, Q = NUM
+C, Q = map(float,input().split())
 if C == "1":
-    print("Total: R$ {:.2f}".format(4.00 * float(Q)))
+    print("Total: R$ {:.2f}".format(4.00 * Q))
 if C == "2":
-    print("Total: R$ {:.2f}".format(4.50 * float(Q)))
+    print("Total: R$ {:.2f}".format(4.50 * Q))
 if C == "3":
-    print("Total: R$ {:.2f}".format(5.00 * float(Q)))
+    print("Total: R$ {:.2f}".format(5.00 * Q))
 if C == "4":
-    print("Total: R$ {:.2f}".format(2.00 * float(Q)))
+    print("Total: R$ {:.2f}".format(2.00 * Q))
 if C == "5":
-    print("Total: R$ {:.2f}".format(1.50 * float(Q)))
+    print("Total: R$ {:.2f}".format(1.50 * Q))
