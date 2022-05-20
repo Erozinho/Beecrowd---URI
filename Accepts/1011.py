e2eff371a3e52b5f0bc103c89036a3c3a93709f5
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 13:29:50 -0300

    Update 1011.py

--- Accepts/1011.py
+++ Accepts/1011.py
@@ -1,4 +1,9 @@
 r = float(input())
-n =  3.14159
-v = (4/3) * n * r**3
-print("VOLUME = {:.3f}".format(v))
+print("VOLUME = {:.3f}".format((4/3) * 3.14159 * r**3))
+
+# Codigo velho:
+
+#r = float(input())
+#n =  3.14159
+#v = (4/3) * n * r**3
+#print("VOLUME = {:.3f}".format(v))
