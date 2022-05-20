3e570931b9eb227d3879906a4383e248c92df8da
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 14:01:25 -0300

    Update 1035.py

--- Accepts/1035.py
+++ Accepts/1035.py
@@ -1,10 +1,18 @@
-x = input().split(" ")
-A, B, C, D = x
-A = int(A)
-B = int(B)
-C = int(C)
-D = int(D)
+A, B, C, D = map(int,input().split())
 if B > C and D > A and (C + D) > (A + B) and C > 0 and D > 0 and A % 2 ==0:
     print("Valores aceitos")
 else:
     print("Valores nao aceitos")
+
+# Codigo velho:
+
+#x = input().split(" ")
+#A, B, C, D = x
+#A = int(A)
+#B = int(B)
+#C = int(C)
+#D = int(D)
+#if B > C and D > A and (C + D) > (A + B) and C > 0 and D > 0 and A % 2 ==0:
+    #print("Valores aceitos")
+#else:
+    #print("Valores nao aceitos")
