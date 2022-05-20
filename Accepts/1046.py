11e5e9e8104cfa0e64e23eb242ea52ab3d1777dd
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 14:59:48 -0300

    Update 1046.py

--- Accepts/1046.py
+++ Accepts/1046.py
@@ -1,9 +1,18 @@
-NUM = input().split()
-A, B = NUM
-A = int(A)
-B = int(B)
+A,B = map(int,input().split())
 if A < B:
     C = B - A
 else:
     C = (24 - A) + B
 print("O JOGO DUROU {} HORA(S)".format(C))
+
+# Codigo velho:
+
+#NUM = input().split()
+#A, B = NUM
+#A = int(A)
+#B = int(B)
+#if A < B:
+    #C = B - A
+#else:
+    #C = (24 - A) + B
+#print("O JOGO DUROU {} HORA(S)".format(C))
