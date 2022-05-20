837d42d9d0420a2e418680148896dd9f79b233f5
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 13:36:50 -0300

    Update 1012.py

--- Accepts/1012.py
+++ Accepts/1012.py
@@ -1,13 +1,22 @@
-VALOR = input().split(" ")
-A, B, C = VALOR
-N = 3.14159
-ATRI = (float(A)) * (float(C)) / 2
-ACIR = N * (float(C)) ** 2
-ATRAP = ((float(B)) + (float(A))) * (float(C)) / 2
-AQUAD = (float(B)) * (float(B))
-ARET = (float(A)) * (float(B))
-print("TRIANGULO: {:.3f}".format(ATRI))
-print("CIRCULO: {:.3f}".format(ACIR))
-print("TRAPEZIO: {:.3f}".format(ATRAP))
-print("QUADRADO: {:.3f}".format(AQUAD))
-print("RETANGULO: {:.3f}".format(ARET))
+A,B,C = map(float,input().split())
+print("TRIANGULO: {:.3f}".format((A * C) / 2))
+print("CIRCULO: {:.3f}".format(3.14159 * C ** 2))
+print("TRAPEZIO: {:.3f}".format(((B + A) * C) / 2))
+print("QUADRADO: {:.3f}".format(B * B))
+print("RETANGULO: {:.3f}".format(A * B))
+
+# Codigo velho:
+
+#VALOR = input().split(" ")
+#A, B, C = VALOR
+#N = 3.14159
+#ATRI = (float(A)) * (float(C)) / 2
+#ACIR = N * (float(C)) ** 2
+#ATRAP = ((float(B)) + (float(A))) * (float(C)) / 2
+#AQUAD = (float(B)) * (float(B))
+#ARET = (float(A)) * (float(B))
+#print("TRIANGULO: {:.3f}".format(ATRI))
+#print("CIRCULO: {:.3f}".format(ACIR))
+#print("TRAPEZIO: {:.3f}".format(ATRAP))
+#print("QUADRADO: {:.3f}".format(AQUAD))
+#print("RETANGULO: {:.3f}".format(ARET))
