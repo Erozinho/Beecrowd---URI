8fae85310d6700522c412b2ac20980af5fd5b5ab
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 13:46:33 -0300

    Update 1015.py

--- Accepts/1015.py
+++ Accepts/1015.py
@@ -1,7 +1,14 @@
 import math
-linha1 = input().split(" ")
-linha2 = input().split(" ")
-x1,y1 = linha1
-x2,y2 = linha2
-raiz = math.sqrt(((float(x2) - float(x1))*(float(x2) - float(x1))) + ((float(y2)-float(y1)) *(float(y2)-float(y1))))
-print("{:.4f}".format(raiz))
+x1,y1 = map(float,input().split())
+x2,y2 = map(float,input().split())
+print("{:.4f}".format(math.sqrt(((x2 - x1)*(x2 - x1)) + ((y2 - y1)) *(y2 - y1))))
+
+# Codigo velho:
+
+#import math
+#linha1 = input().split(" ")
+#linha2 = input().split(" ")
+#x1,y1 = linha1
+#x2,y2 = linha2
+#raiz = math.sqrt(((float(x2) - float(x1))*(float(x2) - float(x1))) + ((float(y2)-float(y1)) *(float(y2)-float(y1))))
+#print("{:.4f}".format(raiz))
