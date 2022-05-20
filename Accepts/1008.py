80220e65c8d8967ffcdc727ae42500f9ad7365bd
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 13:56:09 -0300

    Update 1008.py

--- Accepts/1008.py
+++ Accepts/1008.py
@@ -1,5 +1,3 @@
-NUMBER = int(input())
-HOURS = float(input())
-SALARIOHORA = float(input())
+NUMBER,HOURS,SALARIOHORA = int(input()),float(input()),float(input())
 print("NUMBER = {}".format(NUMBER))
 print("SALARY = U$ {0:.2f}".format((HOURS*SALARIOHORA)))
