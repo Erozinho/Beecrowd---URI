001a39445ba3f6a34c5321ae23c4e6de35c7c730
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 14:42:48 -0300

    Update 1041.py

--- Accepts/1041.py
+++ Accepts/1041.py
@@ -1,15 +1,15 @@
 X,Y = map(float,input().split())
-if x == 0 and y == 0:
+if X == 0 and Y == 0:
     print("Origem")
-if x == 0 and y != 0:
+elif X == 0 and Y != 0:
     print("Eixo Y")
-if x != 0 and y == 0:
+elif X != 0 and Y == 0:
     print("Eixo X")
-if x >= 0 and y >= 0:
+elif X >= 0 and Y >= 0:
     print("Q1")
-if x >= 0 and y <= 0:
+elif X >= 0 and Y <= 0:
     print("Q4")
-if x <= 0 and y <= 0:
+elif X <= 0 and Y <= 0:
     print("Q3")
-if x <= 0 and y >= 0:
+elif X <= 0 and Y >= 0:
     print("Q2")
