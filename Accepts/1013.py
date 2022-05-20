0eb4be76bef08631f64688b0c0732d1f40965114
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-20 13:42:26 -0300

    Update 1013.py

--- Accepts/1013.py
+++ Accepts/1013.py
@@ -1,6 +1,14 @@
 import math
-valor = input().split(" ")
-a, b, c = valor
-maior = (int(a) + int(b) + abs(int(a) - int(b)))  / 2
-resultado = (int(maior) + int(c) + abs(int(maior) - int(c)))/2
-print("{:.0f} eh o maior".format(resultado))
+a,b,c = map(int,input().split())
+maior = ((a + b + abs(a - b))  / 2)
+resultado = (maior + c + abs(maior - c))/2
+print("{:.0f} eh o maior".format((maior + c + abs(maior - c))/2))
+
+# Codigo velho:
+
+#import math
+#valor = input().split(" ")
+#a, b, c = valor
+#maior = (int(a) + int(b) + abs(int(a) - int(b)))  / 2
+#resultado = (int(maior) + int(c) + abs(int(maior) - int(c)))/2
+#print("{:.0f} eh o maior".format(resultado))
