1c69102fb0686a3c3648ff8bec44ab38fce8ae8f
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-24 12:53:27 -0300

    Create 1151(SOLVED).py

---
+++ Trying/1151(SOLVED).py
@@ -0,0 +1,26 @@
+p,s=0,1
+F=[p,s]
+for i in range(2,46):
+    t=s+p
+    p=s
+    s=t
+    F.append(t)
+NUM = int(input())
+for i in range(0, NUM):
+    print("{}".format(F[i]), end=" ")
+
+#CERTO:
+
+#p,s=0,1
+#F=[p,s]
+#for i in range(2,46):
+    #t=s+p
+    #p=s
+    #s=t
+    #F.append(t)
+#NUM = int(input())
+#for i in range(0, NUM):
+    #if i != (NUM - 1):
+        #print("{}".format(F[i]), end=" ")
+    #else:
+        #print("{}".format(F[i]))
