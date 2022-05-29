55e4324dfdd2389732b7a15ae1828654e95a3dc5
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-29 18:44:53 -0300

    Create 2344.py

---
+++ Accepts/2344.py
@@ -0,0 +1,11 @@
+A = int(input())
+if 1 <= A <= 35:
+    print("D")
+elif 36 <= A <= 60:
+    print("C")
+elif 61 <= A <= 85:
+    print("B")
+elif 86 <= A <= 100:
+    print("A")
+else:
+    print("E")
