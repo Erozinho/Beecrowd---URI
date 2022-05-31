c1fc0d2f48e12a74a8f2864a2d059583758ba20a
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-05-31 12:33:28 -0300

    Create 2752.py

---
+++ Accepts/2752.py
@@ -0,0 +1,9 @@
+N = "AMO FAZER EXERCICIO NO URI"
+print("<%s>" % N)
+print("<%30s>" % N)
+print("<%.20s>" % N)
+print("<%-20s>" % N)
+print("<%-30s>" % N)
+print("<%.30s>" % N)
+print("<%30.20s>" % N)
+print("<%-30.20s>" % N)
