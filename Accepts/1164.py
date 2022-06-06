fc93a828d7a9a322a2057b4f5913e3bca498a775
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-06 20:45:50 -0300

    Update 1164.py

--- Accepts/1164.py
+++ Accepts/1164.py
@@ -1,11 +1,7 @@
 VEZES = int(input())
 for X in range(VEZES):
-    S = 0
-    N_VERIFI = int(input())
+    N_VERIFI,S = int(input()),0
     for M in range(1,N_VERIFI):
-        if (N_VERIFI % M == 0):
-            S += M
-    if S == N_VERIFI:
-        print("{} eh perfeito".format(N_VERIFI))
-    else:
-        print("{} nao eh perfeito".format(N_VERIFI))
+        if (N_VERIFI % M == 0): S += M
+    if S == N_VERIFI: print("{} eh perfeito".format(N_VERIFI))
+    else: print("{} nao eh perfeito".format(N_VERIFI))
