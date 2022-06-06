ed485c3286969c5bfd02cfbfc101b0ed8d8bb22b
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-06 01:00:28 -0300

    Update 1166.py

--- Liçoes/1 semestre/AC 2/1166.py
+++ Liçoes/1 semestre/AC 2/1166.py
@@ -1,10 +1,8 @@
 VT,VP = int(input()),int(input())
 AP = round(VT/VP)
 if AP < VT/VP: AP +=1
-if AP > 0 and AP < 1 : print("pagamento: 1\nantes = {}\ndepois = 0\n-----".format(VT))
-else:
-    for x in range(AP):
-        A = VT - VP
-        if A <= 0: A = 0
-        print("pagamento: {}\nantes = {}\ndepois = {}\n-----".format(x+1, VT, A))
-        VT -= VP
+for x in range(AP):
+    A = VT - VP
+    if A <= 0: A = 0
+    print("pagamento: {}\nantes = {}\ndepois = {}\n-----".format(x+1, VT, A))
+    VT -= VP
