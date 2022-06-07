39285848173b6deaee95bb7603178607d60da41f
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-07 19:02:32 -0300

    Create 2861.py

---
+++ Accepts/2861.py
@@ -0,0 +1,8 @@
+while True:
+    try:
+        N_PERGUNTA = int(input())
+        for x in range(N_PERGUNTA):
+            P = input()
+            print("gzuz")
+    except EOFError:
+        break
