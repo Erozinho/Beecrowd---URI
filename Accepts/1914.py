f0ba973f4f8dbcdc02a7b60be3924a2bf6997850
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-09 02:57:50 -0300

    Delete 1914.py

--- Accepts/1914.py
+++
@@ -1,8 +0,0 @@
-TESTE= int(input())
-for x in range(TESTE):
-    LISTA = list(input().split())
-    A,B = map(int,input().split())
-    if LISTA[1] == "PAR" and (A+B)%2==0:print(LISTA[0])
-    elif LISTA[1] == "IMPAR" and (A+B)%2!=0:print(LISTA[2])
-    elif LISTA[3] == "PAR" and (A+B)%2==0:print(LISTA[2])
-    elif LISTA[3] == "IMPAR" and (A+B)%2!=0:print(LISTA[2])
