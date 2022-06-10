118a5cf8752e42942d701402a221e0b32e6fc9bf
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-10 18:28:56 -0300

    Update 1164.py

--- Liçoes/1 semestre/AC 2/1164.py
+++ Liçoes/1 semestre/AC 2/1164.py
@@ -1,6 +1,4 @@
 N = int(input())
 for X in range(N):
-    if X == 0:
-        print("{}".format(chr(65+X)))
-    else:
-        print("{}".format(chr(65+X)*(X+1)))
+    if X == 0:print("{}".format(chr(65+X)))
+    else:print("{}".format(chr(65+X)*(X+1)))
