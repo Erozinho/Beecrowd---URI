4c1e586ea2d48958aece9afcb53d93285d958d83
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-11 23:54:47 -0300

    Update 3301.py

--- Accepts/3301.py
+++ Accepts/3301.py
@@ -1,11 +1,5 @@
 idades = list(map(int,input().split()))
 for x in range(3):
-    if idades[x] != min(idades) and idades[x] != max(idades) and idades.index(idades[x]) == 0:
-        print("huguinho")
-        break
-    if idades[x] != min(idades) and idades[x] != max(idades) and idades.index(idades[x]) == 1:
-        print("zezinho")
-        break
-    if idades[x] != min(idades) and idades[x] != max(idades) and idades.index(idades[x]) == 2:
-        print("luisinho")
-        break
+    if idades[x] != min(idades) and idades[x] != max(idades) and idades.index(idades[x]) == 0:print("huguinho")
+    if idades[x] != min(idades) and idades[x] != max(idades) and idades.index(idades[x]) == 1:print("zezinho")
+    if idades[x] != min(idades) and idades[x] != max(idades) and idades.index(idades[x]) == 2:print("luisinho")
