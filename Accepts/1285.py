c4fb2aac3cc45f8132ed03b2c83d0ae8437e0339
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-17 15:26:46 -0300

    Update 1285.py

--- Accepts/1285.py
+++ Accepts/1285.py
@@ -3,8 +3,7 @@ while True:
         N, M = map(int,input().split())
         NUM = 0
         for X in range(N, M + 1):
-            if len(set(list(str(X)))) == len(str(X)):
-                NUM += 1
+            if len(set(list(str(X)))) == len(str(X)):NUM += 1
         print(NUM)
     except EOFError:
         break
