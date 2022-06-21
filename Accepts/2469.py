8fee7e431cf0bd0317c27b4bff81987ac42b15d1
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-06-21 01:38:47 -0300

    Update 2469.py

--- Accepts/2469.py
+++ Accepts/2469.py
@@ -1,3 +1,6 @@
-NUM = int(input())
-NOTAS = input().split()
-print("{}".format(max(set(NOTAS), key=NOTAS.count)))
+from collections import Counter
+NNOTAS = int(input())
+NOTAS = list(map(int,input().split()))
+NOTAS.sort(reverse=True)
+c = Counter(NOTAS)
+print(c.most_common(1)[0][0])
