c8cc0d3b7f35460524360a6c0dabedd12da45719
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-07-04 20:16:18 -0300

    Update 1980.py

--- Accepts/1980.py
+++ Accepts/1980.py
@@ -1,5 +1,6 @@
-fat = [0, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600, 6227020800, 87178291200, 1307674368000]
 while True:
-    palavra = input()
-    if palavra == "0":quit()
-    print(fat[len(palavra)])
+    EMBA = 1
+    LETRAS = input().split()[0]
+    if LETRAS == "0":break
+    for x in range(1, len(LETRAS)+1):EMBA *= x
+    print(EMBA)
