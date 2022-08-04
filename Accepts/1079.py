5feaad0d44e3e365d7b5d8eeb179f9d5dd18b528
Felipe Araujo <72923499+Erozinho@users.noreply.github.com>
2022-08-04 16:03:06 -0300

    Update 1079.py
    Novo codigo

--- Accepts/1079.py
+++ Accepts/1079.py
@@ -1,8 +1,4 @@
 A = int(input())
-for X in range(1, A + 1):
-    NUM = input().split()
-    N1, N2 , N3 = NUM
-    N1 = float(N1)
-    N2 = float(N2)
-    N3 = float(N3)
+for X in range(A):
+    N1, N2 , N3 = map(float,input().split())
     print("{:.1f}".format(((N1 * 2) + (N2 * 3) + (N3 * 5)) / 10))
